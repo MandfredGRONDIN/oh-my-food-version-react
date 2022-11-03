efb36e3a8d4d471908c16dbf62a176bd627578be
Mandfred GRONDIN <mandfred974@hotmail.fr>
2022-11-03 23:23:37 +0100

    Update
    Rajout du header pour les menus avec son CSS, rajout de la section 1 et 2 dans le menu

--- src/components/Menu/Header.js
+++ src/components/Menu/Header.js
@@ -1,4 +1,15 @@
-export default function Header(data) {
-   console.log(data);
-   return <div>Ceci est un test</div>;
+import { Link } from "react-router-dom";
+import Logo from "../../assets/ohmyfood.png";
+
+export default function Header() {
+   return (
+      <div id="header">
+         <Link to="/" className="back__index">
+            <i className="fa-solid fa-arrow-left"></i>
+         </Link>
+         <div className="header__logo">
+            <img src={Logo} alt="menu"></img>
+         </div>
+      </div>
+   );
 }
--- src/views/Menu.js
+++ src/views/Menu.js
@@ -2,6 +2,8 @@ import React, { useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import Products from "../products.json";
 import Header from "../components/Menu/Header";
+import Heart from "../components/Heart";
+import "../styles/Menu.css";
 
 export default function Menu() {
    const params = useParams();
@@ -18,7 +20,24 @@ export default function Menu() {
       <div>
          {Products.filter((product) => params.id === product.title).map(
             (product, index) => (
-               <Header data={product} key={index} />
+               <div key={index}>
+                  <Header />
+                  <section className="section1">
+                     <img src={product.pictures} alt="menu"></img>
+                  </section>
+                  <section className="section2">
+                     <div className="card__big">
+                        <div className="card__header">
+                           <h1>{product.title}</h1>
+                           <div className="card__heart">
+                              <div className="card__heart-menu">
+                                 <Heart />
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </section>
+               </div>
             )
          )}
       </div>
